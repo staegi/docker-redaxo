66a8ab32f5f2eab4d5ab09bf71d85307ac85e8e5
staegi <staegi@gmail.com>
2019-12-16 21:36:22 +0100

    Created output for every single action

--- docker-run.sh
+++ docker-run.sh
@@ -7,12 +7,22 @@ else
     echo "Redaxo has already been installed"
 fi
 
-chmod +x redaxo/bin/console
+echo "Try to make console file executable"
+chmod a+x redaxo/bin/console
 
-redaxo/bin/console db:set-connection --host=$REDAXO_DATABASE_HOST --database=$REDAXO_DATABASE_NAME --login=$REDAXO_DATABASE_USERNAME --password=$REDAXO_DATABASE_PASSWORD
-redaxo/bin/console setup:check
+if [ -x redaxo/bin/console ]; then
+    echo "Set database connection to $REDAXO_DATABASE_NAME on $REDAXO_DATABASE_HOST"
+    redaxo/bin/console db:set-connection --host=$REDAXO_DATABASE_HOST --database=$REDAXO_DATABASE_NAME --login=$REDAXO_DATABASE_USERNAME --password=$REDAXO_DATABASE_PASSWORD
 
+    echo "Check setup"
+    redaxo/bin/console setup:check
+else
+    echo "Console file is not executable"
+fi
+
+echo "Correcting file permissions"
 chown -R www-data:www-data .
 chmod -R a+w,g+s .
 
+echo "Finally launch FPM"
 php-fpm
